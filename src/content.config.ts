91c6afae25e7f4c27c9872c9dd2f05d43381e46e
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-08-29 15:23:36 +0700

    feat: add id and remove optional updatedDate

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,23 +7,25 @@ import { string, z } from "astro/zod";
 const project = defineCollection({
   loader: glob({ base: "./content/project", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
+    id: z.string(),
     name: z.string(),
     description: z.string(),
     url: z.string(),
     languages: z.array(string()),
     pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
+    updatedDate: z.coerce.date(),
   }),
 });
 
 const post = defineCollection({
   loader: glob({ base: "./content/post", pattern: "**/*.{md,mdx}" }),
   schema: z.object({
+    id: z.string(),
     title: z.string(),
     description: z.string(),
     tags: z.array(string()),
     pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
+    updatedDate: z.coerce.date(),
   }),
 });
 
