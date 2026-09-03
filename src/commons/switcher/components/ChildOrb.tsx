4b902106fc22258ca648cb29c09fb4bbfd935c0e
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-04 01:19:58 +0700

    fix: apply css properties

--- src/commons/switcher/components/ChildOrb.tsx
+++ src/commons/switcher/components/ChildOrb.tsx
@@ -1,75 +1,104 @@
 import { createEffect, onCleanup } from "solid-js";
 import { gsap } from "gsap";
 
-export interface orbPosition {
-  x: number;
-  y: number;
-  r: number;
-}
-
 interface ChildOrbProps {
   name: string;
   link: string;
   order: number;
-  base: orbPosition;
   total: number;
   open: boolean;
 }
 
 export default function ChildOrb(props: ChildOrbProps) {
   let ref: HTMLDivElement | undefined;
+  let animation: gsap.core.Tween | undefined;
+
+  const getRadius = () => {
+    if (!ref) return 180;
+
+    const value = getComputedStyle(ref).getPropertyValue("--orb-r");
+
+    return parseFloat(value) || 180;
+  };
 
   const getTargetPosition = () => {
     const angle = (Math.PI / 2) * (props.order / (props.total - 1));
 
-    const x = props.base.r * Math.cos(angle);
-    const y = -props.base.r * Math.sin(angle);
+    const r = getRadius();
 
-    return { x, y };
+    return {
+      x: r * Math.cos(angle),
+      y: -r * Math.sin(angle),
+    };
   };
 
   createEffect(() => {
     if (!ref) return;
-    gsap.killTweensOf(ref);
 
-    if (props.open) {
+    if (!animation) {
       const { x, y } = getTargetPosition();
-      gsap.fromTo(
-        ref,
-        { x: 0, y: 0, xPercent: -50, yPercent: -50, opacity: 0, scale: 0.5 },
-        {
-          x,
-          y,
-          xPercent: -50,
-          yPercent: -50,
-          opacity: 1,
-          scale: 1,
-          duration: 0.8,
-          delay: props.order * 0.05,
-          ease: "back.out(1.7)",
-        },
-      );
-    } else {
-      gsap.to(ref, {
+
+      gsap.set(ref, {
         x: 0,
         y: 0,
+        xPercent: -50,
+        yPercent: -50,
         opacity: 0,
-        scale: 0.5,
-        duration: 0.5,
-        delay: (props.total - props.order) * 0.03,
-        ease: "power2.in",
+        scale: 1,
       });
+
+      animation = gsap.to(ref, {
+        x,
+        y,
+        xPercent: -50,
+        yPercent: -50,
+        opacity: 1,
+        scale: 1,
+        duration: 0.8,
+        delay: 0.05,
+        ease: "back.out(1.8)",
+        paused: true,
+      });
+    }
+
+    if (props.open) {
+      animation.play();
+    } else if (animation.progress() > 0) {
+      animation.reverse();
     }
   });
 
   onCleanup(() => {
-    if (ref) gsap.killTweensOf(ref);
+    animation?.kill();
   });
 
   return (
     <div
       ref={ref}
-      class="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 pointer-events-auto border cursor-pointer rounded-full h-22 w-22 flex items-center justify-center"
+      class="
+        absolute top-0 left-0
+        -translate-x-1/2 -translate-y-1/2
+        rounded-full
+        border cursor-pointer
+
+        h-20 w-20
+        [--orb-r:140em]
+
+        sm:h-20 sm:w-20
+        sm:[--orb-r:160em]
+
+        md:h-20 md:w-20
+        md:[--orb-r:170em]
+
+        lg:h-22 lg:w-22
+        lg:[--orb-r:180em]
+
+        flex items-center justify-center
+      "
+      classList={{
+        "pointer-events-auto": props.open,
+        "pointer-events-none": !props.open,
+      }}
     >
       <a href={props.link}>{props.name}</a>
     </div>
