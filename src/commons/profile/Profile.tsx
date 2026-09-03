2b772c74d787dcac432543141c2511f170791e83
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-03 16:15:23 +0700

    feat: adding profile image inside post

--- src/commons/profile/Profile.tsx
+++ src/commons/profile/Profile.tsx
@@ -1,9 +1,11 @@
 import SocialNetwork from "./components/SocialNetwork";
 import TagItem from "./components/TagItem";
+import Face from "../../assets/Face.jpeg?url";
 
 export default function Profile() {
   return (
-    <div>
+    <div class="">
+      <img src={Face} class="w-full aspect-square object-cover rounded-full" />
       <SocialNetwork />
       <TagItem />
     </div>
