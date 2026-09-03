53d872ce60d3f0ca09c0debcce8e90a54e325f06
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-03 16:15:40 +0700

    feat: placing search at the center of the banner

--- src/commons/search/Search.tsx
+++ src/commons/search/Search.tsx
@@ -1,7 +1,11 @@
 export default function Search() {
   return (
-    <div class="fixed top-5 right-12 border">
-      <input type="text" placeholder="Search" />
+    <div class="absolute inset-1 flex items-center justify-center">
+      <input
+        type="text"
+        placeholder="Search"
+        class="w-[60%] h-12 px-5 text-lg rounded-full text-white border"
+      />
     </div>
   );
 }
