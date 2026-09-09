1cd41a3661cb6bd633689afafb04b2590e58d3e5
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-09 16:01:24 +0700

    fix: remove pertentage width

--- src/pages/project/components/_Analysis.tsx
+++ src/pages/project/components/_Analysis.tsx
@@ -1,3 +1,7 @@
 export default function Analysis() {
-  return <div class="">Analysis</div>;
+  return (
+    <div class="sticky top-5 h-fit m-5 rounded-2xl p-5 bg-gray-700">
+      Analysis
+    </div>
+  );
 }
--- src/pages/project/components/_ProjectCard.tsx
+++ src/pages/project/components/_ProjectCard.tsx
@@ -8,15 +8,32 @@ export default function projectCard({ project }: Props) {
   const content = project.data;
 
   return (
-    <div class="w-[95%] mt-5 p-5">
-      <div class="flex flex-row">
-        <p> {content.name}</p>
-        <a href={content.url}>icon</a>
+    <div class="w-full p-5 bg-gray-600 rounded-t-2xl">
+      <div>
+        <div class="flex flex-row justify-between">
+          <div class="flex flex-row gap-6">
+            <p class="font-bold text-2xl"> {content.name}</p>
+            <a href={content.url}>🔗</a>
+          </div>
+          <div class=""> {content.pubDate.toDateString()}</div>
+        </div>
+
+        <div class="flex flex-col flex-wrap mt-3">
+          {" "}
+          <span class="font-bold">Description: </span>
+          <p> {content.description}</p>
+        </div>
+
+        <div class="flex flex-col flex-wrap mt-3">
+          {" "}
+          <span class="font-bold">Languages: </span>
+          <div class="flex flex-row gap-3">
+            {content.languages.map((item) => (
+              <p>{item} </p>
+            ))}
+          </div>
+        </div>
       </div>
-      <div> {content.description}</div>
-      <div> {content.url}</div>
-      <div> {content.languages}</div>
-      <div> {content.pubDate.toDateString()}</div>
     </div>
   );
 }
