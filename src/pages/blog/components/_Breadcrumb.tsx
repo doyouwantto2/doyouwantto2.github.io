623b20fe6cfb1d95e8376f3546f542768e3bf2d1
doyouwantto2 <92518098+doyouwantto2@users.noreply.github.com>
2026-09-11 23:51:54 +0700

    feat: add green text

--- src/pages/blog/components/_Breadcrumb.tsx
+++ src/pages/blog/components/_Breadcrumb.tsx
@@ -7,7 +7,7 @@ interface BreadcrumbProps {
 
 export default function Breadcrumb({ link, name, id, title }: BreadcrumbProps) {
   const linkClass =
-    "rounded-xl pl-3 pr-3 pb-2 pt-2 w-fit mb-3 inline-block text-xl font-bold";
+    "text-green-400 rounded-xl pl-3 pr-3 pb-2 pt-2 w-fit mb-3 inline-block text-xl font-bold";
 
   return (
     <div>
